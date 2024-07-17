125996d7ee157af811ebdd81adbca4723f649630
Kayden <Kayden@test.com>
2024-07-17 10:07:14 +0800

    Signup and Login

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,19 +1,25 @@
 const asyncHandler = require("express-async-handler");
 
-const { signup } = require("../services/auth.service");
+const { signup, login } = require("../services/auth.service");
 
 exports.signup = asyncHandler(async (req, res, next) => {
   try {
     const { username, password } = req.body;
     await signup(username, password);
-    res.send("Signup success!");
+    res.status(200).json({ message: "Signup OK!" });
   } catch (e) {
-    res.send(`Signup failed... ${e}`);
+    res.status(400).json({ message: `Signup failed... ${e}` });
   }
 });
 
 exports.login = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Login");
+  try {
+    const { username, password } = req.body;
+    await login(username, password);
+    res.status(200).json({ message: "Login OK!" });
+  } catch (e) {
+    res.status(400).json({ message: `Login failed... ${e}` });
+  }
 });
 
 exports.logout = asyncHandler(async (req, res, next) => {
--- services/auth.service.js
+++ services/auth.service.js
@@ -8,21 +8,38 @@ const getUsers = () => {
 };
 
 const updateUsers = (users, user) => {
+  if (getUser(users, user) != null) {
+    throw "Username already exists";
+  }
   users.push(user);
   fs.writeFileSync(path, JSON.stringify(users));
 };
 
-const signup = async (u, p) => {
-  const user = { username: u, password: p };
+const getUser = (users, user) => {
+  return users.find((u) => u.username == user.username);
+};
 
+exports.signup = async (u, p) => {
+  const req = { username: u, password: p };
   const users = getUsers();
+
   try {
-    updateUsers(users, user);
+    updateUsers(users, req);
   } catch (e) {
-    console.log(e);
+    throw e;
   }
 };
 
-module.exports = {
-  signup,
+exports.login = async (u, p) => {
+  const req = { username: u, password: p };
+  const users = getUsers();
+  const user = getUser(users, req);
+
+  if (user == null) {
+    throw "Invalid Username or Password.";
+  }
+
+  if (user.password != req.password) {
+    throw "Invalid Username or Password.";
+  }
 };
